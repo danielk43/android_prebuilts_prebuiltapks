09e49c85414c8bdadb0af5a0730e1d68a1fbe7dd
danielk43 <kun@buffalo.edu>
2019-11-24 09:22:04 -0700

    Updated FDroid privapp and EdXposedManager

--- EdXposedManager/Android.mk
+++ EdXposedManager/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := EdXposedManager
-LOCAL_SRC_FILES := EdXposedManager-org.meowcat.edxposed.manager-4.5.0-450-release.apk
+LOCAL_SRC_FILES := EdXposedManager-org.meowcat.edxposed.manager-4.5.2-452-release.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
