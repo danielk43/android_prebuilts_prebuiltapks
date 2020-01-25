492d32d6e636f109939a37e771b444213a996f8c
danielk43 <kun@buffalo.edu>
2020-01-24 21:14:17 -0700

    Update AuroraServices mk file

--- AuroraServices/Android.mk
+++ AuroraServices/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := privapp-permissions-AuroraServices.privileged.xml
+LOCAL_MODULE := permissions_com.aurora.services.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
@@ -16,6 +16,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REQUIRED_MODULES := privapp-permissions-org.fdroid.fdroid.privileged.xml
+LOCAL_REQUIRED_MODULES := permissions_com.aurora.services.xml
 include $(BUILD_PREBUILT)
 
